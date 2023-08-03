3fc7a7093bf102a088e96adf2a29e5d4e93231ad
Markus Rytter <markus.r@live.dk>
2023-08-03 17:57:27 +0200

    Formatted file

--- src/neomesh.h
+++ src/neomesh.h
@@ -11,7 +11,7 @@
  * @addtogroup NeoMesh
  * @{
  */
- 
+
 #ifndef NEOMESH_H
 #define NEOMESH_H
 
@@ -22,7 +22,6 @@
 #include <HardwareSerial.h>
 #include <Arduino.h>
 #include "NcApi.h"
-
 
 /*******************************************************************************
  *    Defines
@@ -48,22 +47,21 @@
 #define NODE_ID_SETTING 0xA
 
 #if defined(UBRRH) || defined(UBRR0H)
-  extern HardwareSerial Serial;
-  #define HAVE_HWSERIAL0
+extern HardwareSerial Serial;
+#define HAVE_HWSERIAL0
 #endif
 #if defined(UBRR1H)
-  extern HardwareSerial Serial1;
-  #define HAVE_HWSERIAL1
+extern HardwareSerial Serial1;
+#define HAVE_HWSERIAL1
 #endif
 #if defined(UBRR2H)
-  extern HardwareSerial Serial2;
-  #define HAVE_HWSERIAL2
+extern HardwareSerial Serial2;
+#define HAVE_HWSERIAL2
 #endif
 #if defined(UBRR3H)
-  extern HardwareSerial Serial3;
-  #define HAVE_HWSERIAL3
+extern HardwareSerial Serial3;
+#define HAVE_HWSERIAL3
 #endif
-
 
 /*******************************************************************************
  *    Class prototypes
@@ -75,127 +73,126 @@
 class NeoMesh
 {
 public:
-  /**
-   * @brief Construct new NeoMesh object
-   * @param uart_num Which UART is connected to the AAPI UART of the NeoCortec module
-   * @param cts_pin The GPIO connected to the cts pin of the NeoCortec module
-   */
-  NeoMesh(uint8_t uart_num, uint8_t cts_pin);
+    /**
+     * @brief Construct new NeoMesh object
+     * @param uart_num Which UART is connected to the AAPI UART of the NeoCortec module
+     * @param cts_pin The GPIO connected to the cts pin of the NeoCortec module
+     */
+    NeoMesh(uint8_t uart_num, uint8_t cts_pin);
 
-  /**
-   * @brief Starts the NeoMesh API
-   */
-  void start();
+    /**
+     * @brief Starts the NeoMesh API
+     */
+    void start();
 
-  // IGNORE:
-  void write(uint8_t * finalMsg, uint8_t finalMsgLength);
+    // IGNORE:
+    void write(uint8_t *finalMsg, uint8_t finalMsgLength);
 
-  /**
-   * @brief Handles all housekeeping. Should be called from main loop
-   */
-  void update();
+    /**
+     * @brief Handles all housekeeping. Should be called from main loop
+     */
+    void update();
 
-  /**
-   * @brief Change the id of the node in the NeoMesh network
-   * When the ID of a node is changed, it will not revert on reboot.
-   * The ID is saved safely within the NeoCortec module.
-   * This function reboots the NeoCortec module, so it will not be
-   * possible to send data from this node for a period of time after calling this function
-   * @param node_id The new nodeid. NOTE: Can not be 0
-   */
-  void change_node_id(uint16_t node_id);
+    /**
+     * @brief Change the id of the node in the NeoMesh network
+     * When the ID of a node is changed, it will not revert on reboot.
+     * The ID is saved safely within the NeoCortec module.
+     * This function reboots the NeoCortec module, so it will not be
+     * possible to send data from this node for a period of time after calling this function
+     * @param node_id The new nodeid. NOTE: Can not be 0
+     */
+    void change_node_id(uint16_t node_id);
 
-  /**
-   * @brief Change the network id
-   * Change the network ID setting within the NeoCortec module.
-   * As goes for the node id, the network id is not reverted on reboot.
-   * Alle nodes in a network must have the same network id in order to communicate
-   * @param network_id The new network id as 16 bytes
-   */
-  void change_network_id(uint8_t network_id[16]);
+    /**
+     * @brief Change the network id
+     * Change the network ID setting within the NeoCortec module.
+     * As goes for the node id, the network id is not reverted on reboot.
+     * Alle nodes in a network must have the same network id in order to communicate
+     * @param network_id The new network id as 16 bytes
+     */
+    void change_network_id(uint8_t network_id[16]);
 
-  /**
-   * @brief Change baudrate (Must be called before start)
-   * If the module is configured to use a dfferent baudrate than 115200,
-   * this function must be called with the custom baudrate before the start function is called
-   */
-  void set_baudrate(uint32_t baudrate);
+    /**
+     * @brief Change baudrate (Must be called before start)
+     * If the module is configured to use a dfferent baudrate than 115200,
+     * this function must be called with the custom baudrate before the start function is called
+     */
+    void set_baudrate(uint32_t baudrate);
 
-  /**
-   * @brief send an unacknowledged message to a node in the network
-   * @param destNodeId The node id of the recepient
-   * @param port Which port to send to. Allows recepient to filter messages. If not used, write 0
-   * @param appSeqNo message sequence number. If more messages are sent after each other, the sequence number must be different each time
-   * @param payload The payload data to send
-   * @param payloadLen The length of the payload array
-   */
-  void send_unacknowledged(uint16_t destNodeId, uint8_t port, uint16_t appSeqNo, uint8_t * payload, uint8_t payloadLen);
-  
-  /**
-   * @brief send an acknowledged message to a node in the network
-   * If the host_ack_callback is set it will be called when the message recepient has acknowledged
-   * @param destNodeId The node id of the recepient
-   * @param port Which port to send to. Allows recepient to filter messages. If not used, write 0
-   * @param payload The payload data to send
-   * @param payloadLen The length of the payload array
-   */
-  void send_acknowledged(uint16_t destNodeId, uint8_t port, uint8_t * payload, uint8_t payloadLen);
+    /**
+     * @brief send an unacknowledged message to a node in the network
+     * @param destNodeId The node id of the recepient
+     * @param port Which port to send to. Allows recepient to filter messages. If not used, write 0
+     * @param appSeqNo message sequence number. If more messages are sent after each other, the sequence number must be different each time
+     * @param payload The payload data to send
+     * @param payloadLen The length of the payload array
+     */
+    void send_unacknowledged(uint16_t destNodeId, uint8_t port, uint16_t appSeqNo, uint8_t *payload, uint8_t payloadLen);
 
-  /**
-   * @brief Send a WES command to the node
-   * @param cmd The command
-   */
-  void send_wes_command(NcApiWesCmdValues cmd);
+    /**
+     * @brief send an acknowledged message to a node in the network
+     * If the host_ack_callback is set it will be called when the message recepient has acknowledged
+     * @param destNodeId The node id of the recepient
+     * @param port Which port to send to. Allows recepient to filter messages. If not used, write 0
+     * @param payload The payload data to send
+     * @param payloadLen The length of the payload array
+     */
+    void send_acknowledged(uint16_t destNodeId, uint8_t port, uint8_t *payload, uint8_t payloadLen);
 
-  /**
-   * @brief Send a wes response
-   * @param uid
-   * @param nodeId
-   */
-  void send_wes_respond(uint64_t uid, uint16_t nodeId);
+    /**
+     * @brief Send a WES command to the node
+     * @param cmd The command
+     */
+    void send_wes_command(NcApiWesCmdValues cmd);
 
-  pfnNcApiReadCallback read_callback = 0;
-  pfnNcApiHostAckCallback host_ack_callback = 0;
-  pfnNcApiHostAckCallback host_nack_callback = 0;
-  pfnNcApiHostDataCallback host_data_callback = 0;
-  pfnNcApiHostDataHapaCallback host_data_hapa_callback = 0;
-  pfnNcApiWesSetupRequestCallback wes_setup_request_callback = 0;
-  pfnNcApiWesStatusCallback wes_status_callback = 0;
+    /**
+     * @brief Send a wes response
+     * @param uid
+     * @param nodeId
+     */
+    void send_wes_respond(uint64_t uid, uint16_t nodeId);
 
-  // IGNORE:
-  void message_written();
-  static void pass_through_cts0();
-  static void pass_through_cts1();
-  static void pass_through_cts2();
-  static void pass_through_cts3();
+    pfnNcApiReadCallback read_callback = 0;
+    pfnNcApiHostAckCallback host_ack_callback = 0;
+    pfnNcApiHostAckCallback host_nack_callback = 0;
+    pfnNcApiHostDataCallback host_data_callback = 0;
+    pfnNcApiHostDataHapaCallback host_data_hapa_callback = 0;
+    pfnNcApiWesSetupRequestCallback wes_setup_request_callback = 0;
+    pfnNcApiWesStatusCallback wes_status_callback = 0;
+
+    // IGNORE:
+    void message_written();
+    static void pass_through_cts0();
+    static void pass_through_cts1();
+    static void pass_through_cts2();
+    static void pass_through_cts3();
 
 private:
-  uint8_t uart_num;
-  uint8_t cts_pin;
-  uint32_t baudrate = DEFAULT_NEOCORTEC_BAUDRATE;
-  HardwareSerial * serial;
+    uint8_t uart_num;
+    uint8_t cts_pin;
+    uint32_t baudrate = DEFAULT_NEOCORTEC_BAUDRATE;
+    HardwareSerial *serial;
 
-  bool _message_written = false;
+    bool _message_written = false;
 
-  void wait_for_message_written();
-  void switch_sapi_aapi();
-  void login_sapi();
-  void change_node_id_sapi(uint16_t nodeid);
-  void write_raw(uint8_t * data, uint8_t length);
-  void start_bootloader();
-  void start_protocol_stack();
-  uint8_t get_setting(uint8_t setting);
-  uint8_t set_setting(uint8_t setting, uint8_t * setting_value, uint8_t setting_value_length);
-  uint8_t commit_settings();
+    void wait_for_message_written();
+    void switch_sapi_aapi();
+    void login_sapi();
+    void change_node_id_sapi(uint16_t nodeid);
+    void write_raw(uint8_t *data, uint8_t length);
+    void start_bootloader();
+    void start_protocol_stack();
+    uint8_t get_setting(uint8_t setting);
+    uint8_t set_setting(uint8_t setting, uint8_t *setting_value, uint8_t setting_value_length);
+    uint8_t commit_settings();
 
-  static pfnNcApiReadCallback read_callback_(uint8_t n, uint8_t * msg, uint8_t msgLength);
-  static pfnNcApiHostAckCallback host_ack_callback_(uint8_t n, tNcApiHostAckNack * p);
-  static pfnNcApiHostAckCallback host_nack_callback_(uint8_t n, tNcApiHostAckNack * p);
-  static pfnNcApiHostDataCallback host_data_callback_(uint8_t n, tNcApiHostData * m);
-  static pfnNcApiHostDataHapaCallback host_data_hapa_callback_(uint8_t n, tNcApiHostDataHapa * p);
-  static pfnNcApiWesSetupRequestCallback wes_setup_request_callback_(uint8_t n, tNcApiWesSetupRequest * p);
-  static pfnNcApiWesStatusCallback wes_status_callback_(uint8_t n, tNcApiWesStatus * p);
-
+    static pfnNcApiReadCallback read_callback_(uint8_t n, uint8_t *msg, uint8_t msgLength);
+    static pfnNcApiHostAckCallback host_ack_callback_(uint8_t n, tNcApiHostAckNack *p);
+    static pfnNcApiHostAckCallback host_nack_callback_(uint8_t n, tNcApiHostAckNack *p);
+    static pfnNcApiHostDataCallback host_data_callback_(uint8_t n, tNcApiHostData *m);
+    static pfnNcApiHostDataHapaCallback host_data_hapa_callback_(uint8_t n, tNcApiHostDataHapa *p);
+    static pfnNcApiWesSetupRequestCallback wes_setup_request_callback_(uint8_t n, tNcApiWesSetupRequest *p);
+    static pfnNcApiWesStatusCallback wes_status_callback_(uint8_t n, tNcApiWesStatus *p);
 };
 
 /*******************************************************************************/
